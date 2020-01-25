08b542d7fb0569ed59fe8ee31a561c81d958b231
sss1 <sss1@andrew.cmu.edu>
2020-01-25 15:01:24 -0500

    Style improvements.

--- analysis_code/preprocess_and_compute_HMMs.py
+++ analysis_code/preprocess_and_compute_HMMs.py
@@ -1,15 +1,15 @@
-import numpy as np
+"""This script preprocesses the data and fits the HMM to each trial."""
+
 import pickle
 
 import load_subjects as ls
 from eyetracking_hmm import performance_according_to_HMM
-from util import impute_missing_data, break_eyetracking_into_trials, interpolate_trackit_to_eyetracking, filter_experiment
+import util
 
-sigma = 300
-sigma2 = sigma ** 2
+_SIGMA = 300
 
 # Output pickle file to which to save results
-save_file = str(sigma) + '.pickle'
+_SAVE_FILE = str(_SIGMA) + '.pickle'
 
 # Load all experiment data
 subjects = ls.load_dataset('shrinky', 'eyetrack')
@@ -21,30 +21,36 @@ print('Merging and preprocessing datasets...')
 # Combine eyetracking with trackit data and perform all preprocessing
 for subject in subjects.values():
   for (experiment_ID, experiment) in subject.experiments.items():
-    impute_missing_data(experiment)
-    break_eyetracking_into_trials(experiment)
-    interpolate_trackit_to_eyetracking(experiment)
-    filter_experiment(experiment)
+    util.impute_missing_data(experiment)
+    util.break_eyetracking_into_trials(experiment)
+    util.interpolate_trackit_to_eyetracking(experiment)
+    util.filter_experiment(experiment)
 
-# Retain only subjects with at least half non-missing data in at least half their trials, in both conditions
+# Retain only subjects with at least half non-missing data in at least half
+# their trials, in both conditions
 def subject_is_good(subject):
-  return len(subject.experiments['shrinky'].trials_to_keep) >= 5 and len(subject.experiments['noshrinky'].trials_to_keep) >= 5
+  return (len(subject.experiments['shrinky'].trials_to_keep) >= 5
+          and len(subject.experiments['noshrinky'].trials_to_keep) >= 5)
 
 # Filter out subjects with too much missing data
-good_subjects = { subject_ID : subject for (subject_ID, subject) in subjects.items() if subject_is_good(subject) }
+good_subjects = {subject_ID : subject
+                 for (subject_ID, subject) in subjects.items()
+                 if subject_is_good(subject)}
 print(str(len(good_subjects)) + ' good subjects: ' + str(good_subjects.keys()))
 bad_subjects = set(subjects.keys()) - set(good_subjects.keys())
 print(str(len(bad_subjects)) + ' bad subjects: ' + str(bad_subjects))
 
 for subject in good_subjects.values():
-  for trial in zip(subject.experiments['shrinky'].datatypes['trackit'].trials,
-                   subject.experiments['shrinky'].datatypes['eyetrack'].trials):
-    performance_according_to_HMM(*trial, sigma2=sigma2)
-  for trial in zip(subject.experiments['noshrinky'].datatypes['trackit'].trials,
-                   subject.experiments['noshrinky'].datatypes['eyetrack'].trials):
-    performance_according_to_HMM(*trial, sigma2=sigma2)
+  experiment = subject.experiments['shrinky']
+  for trial in zip(experiment.datatypes['trackit'].trials,
+                   experiment.datatypes['eyetrack'].trials):
+    performance_according_to_HMM(*trial, sigma2=_SIGMA**2)
+  experiment = subject.experiments['noshrinky']
+  for trial in zip(experiment.datatypes['trackit'].trials,
+                   experiment.datatypes['eyetrack'].trials):
+    performance_according_to_HMM(*trial, sigma2=_SIGMA**2)
 
 # Save results with Pickle
-with open(save_file , 'wb') as f:
-  pickle.dump(subjects, f)
-print('Saved subjects to file \''  + save_file + '\'')
+with open(_SAVE_FILE, 'wb') as fout:
+  pickle.dump(subjects, fout)
+print('Saved subjects to file \''  + _SAVE_FILE + '\'')
